426e52fd4c95538fbc18b9ed754c472ababbe741
TheProfanedOne <mjm11102001@gmail.com>
2022-10-13 19:10:35 -0600

    More Stuff

--- src/scripts/contact_form.js
+++ src/scripts/contact_form.js
@@ -4,11 +4,15 @@ function setForm() {
     document.forms[0].onsubmit = function() {
         if (this.checkValidity()) {
             const data = new FormData(document.forms[0]);
-            const msgInfo = data.entries();
-            for (const [key, value] of msgInfo) {
-                console.log(key, value);
-            }
+            const pairs = {};
+
+            data.forEach((value, key) => {
+                pairs[key] = value;
+            });
+            console.log(pairs);
+
             alert("Email sent!");
+            document.forms[0].reset();
         }
         return false;
     }
